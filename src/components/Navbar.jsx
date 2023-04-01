f67d2e9b4478883e2064ba48bb85aba384dc60dc
mnuralim <shadownur345.com>
2023-04-01 22:38:37 +0800

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-primary bg-primary sticky-top">
         <div className="container-fluid">
-          <Link to={"/"} className="navbar-brand text-white active" href="#">
+          <Link to={"/"} className="navbar-brand text-white active">
             HMPS ILKOM
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,35 +15,35 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link to={"/"} className="nav-link transform" aria-current="page" href="index.html">
+                <Link to={"/"} className="nav-link transform" aria-current="page">
                   Home
                 </Link>
               </li>
               <li className="nav-item dropdown">
-                <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <Link className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Profil
                 </Link>
                 <ul className="dropdown-menu">
                   <li>
-                    <Link to={"/profile"} className="dropdown-item" href="profil.html" title="profil">
+                    <Link to={"/profile"} className="dropdown-item" title="profil">
                       visi, Misi dan Sejarah
                     </Link>
                   </li>
 
                   <li>
-                    <Link to={"/profile"} className="dropdown-item" href="profil.html">
+                    <Link to={"/profile"} className="dropdown-item">
                       Struktur Organisasi
                     </Link>
                   </li>
                 </ul>
               </li>
               <li className="nav-item dropdown">
-                <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <Link className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Galeri
                 </Link>
                 <ul className="dropdown-menu">
                   <li>
-                    <Link to={"/gallery"} className="dropdown-item" href="#">
+                    <Link to={"/gallery"} className="dropdown-item">
                       Galeri foto & video
                     </Link>
                   </li>
@@ -55,7 +55,7 @@ const Navbar = () => {
                 </Link>
               </li>
               <li className="nav-item dropdown">
-                <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <Link className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Hubungi Kami
                 </Link>
                 <ul className="dropdown-menu">
@@ -74,7 +74,7 @@ const Navbar = () => {
             </ul>
             <form className="search-box d-flex" role="search">
               <input className="w-100 ms-3 me-2" type="search" placeholder="Search" aria-label="Search" />
-              <a href="#" title="ini">
+              <a title="ini">
                 <i className="text-dark mt-2 me-3 fa-sharp fa-solid fa-magnifying-glass"></i>
               </a>
             </form>
